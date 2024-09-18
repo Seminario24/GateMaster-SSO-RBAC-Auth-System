36136b91ca41a05fc6aed80aacead229a46d2fcc
JosueEmanuelDionicioOrellana <Jdionicioo1@miumg.edu.gt>
2024-09-17 22:03:27 -0600

    Se crearon servicios para la creacion de usuarios

--- gatemaster/app.js
+++ gatemaster/app.js
@@ -60,6 +60,116 @@ app.get("/apps", authorizationMiddleware("admin"), (req, res) => {
   res.send("Listado de las apps.");
 });
 
+
+app.post("/createuser", async (req, res) => {
+  const {id, username, email, firstName, lastName, password } = req.body;
+
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.status(401).send("Missing authorization header");
+  const token = authHeader.split(" ")[1];
+  try {
+    const { data } = await axios({
+      method: "post",
+      url: `${process.env.KEYCLOAK_AUTH_SERVER_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users`,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+      data: {
+        id,
+        username,
+        email,
+        firstName,
+        lastName,
+        enabled: true,
+        credentials: [
+          {
+            type: 'password',
+            secretData: JSON.stringify({
+              value: password, 
+            }),
+            credentialData: JSON.stringify({
+              algorithm: 'bcrypt',
+              hashIterations: 10,
+            }),
+          },
+        ],
+      },
+    });
+    res.status(201).send("Usuario creado exitosamente");
+    
+  } catch (err) {
+    console.error("Error details: "+ err.response + err.response.data + err.message);
+    res.status(401).send("Ocurrió un error");
+  }
+});
+
+
+app.post("/getuser", async (req, res) => {
+  const { username } = req.body;
+
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.status(401).send("Missing authorization header");
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const { data } = await axios({
+      method: "get",
+      url: `${process.env.KEYCLOAK_AUTH_SERVER_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users?username=${username}`,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+
+    if (data.length > 0) {
+      res.status(200).json(data[0]);  // Devolviendo el primer resultado
+    } else {
+      res.status(404).send("Usuario no encontrado");
+    }
+
+  } catch (err) {
+    console.error("Error details: " + err.response?.data + err.message);
+    res.status(500).send("Ocurrió un error al buscar el usuario");
+  }
+});
+
+app.post("/setuserpassword", async (req, res) => {
+  const { id, password } = req.body;
+
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.status(401).send("Missing authorization header");
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const { status } = await axios({
+      method: "put",
+      url: `${process.env.KEYCLOAK_AUTH_SERVER_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${id}/reset-password`,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+      data: {
+        type: "password",
+        temporary: false, // Cambiar a true si quieres que sea temporal
+        value: password,
+      },
+    });
+
+    if (status === 204) {
+      res.status(200).send("Contraseña actualizada exitosamente");
+    } else {
+      res.status(500).send("Ocurrió un error al actualizar la contraseña");
+    }
+
+  } catch (err) {
+    console.error("Error details: "+ err.response + err.response.data + err.message);
+    res.status(500).send("Ocurrió un error al procesar la solicitud");
+  }
+});
+
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`gatemaster listening on port ${port}`);
